1e5982767dcf0c6a2c1fb98c8ad88ba1132385cd
EFRAIM DE JESUS FARIAS COSTA <efraim.costa@a.ucb.br>
2024-03-13 10:05:18 -0300

    exercício 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,12 @@
+const n1 = 3
+const n2 = 7
+
+soma = n1 + n2
+sub = n1 - n2
+mult = n1 * n2
+div = n1 / n2
+
+console.log("O resultado da soma entre " + n1 + " e " + n2 + " é " + soma)
+console.log("O resultado da soma entre " + n1 + " e " + n2 + " é " + sub)
+console.log(`O resultado da multiplicação entre $(n1) e $(n2) é $(mult)`)
+console.log("O resultado da soma entre " + n1 + " e " + n2 + " é " + div)
