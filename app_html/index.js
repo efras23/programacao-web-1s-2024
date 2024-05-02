9f182b73450973a71e30605042961514192a640a
Efraim <efra10_efraim@hotmail.com>
2024-05-01 22:22:32 -0300

    aula de mustache - renderizar páginas HTML dinâmicas

---
+++ app_html/index.js
@@ -0,0 +1,22 @@
+//Aula 24/04/2024 - Mustache, para mostar páginas html dinâmicas de fato
+
+const express = require("express")
+const mustacheExpress = require("mustache-express")
+const app = express()
+
+app.engine("html", mustacheExpress())
+app.set("view engine", "html")
+app.set("views", __dirname + "/views")
+
+app.get("/", function(req, res){
+    res.render("index.html")
+})
+
+app.get("/contato", function(req, res){
+    res.render("contato.html")
+})
+
+const PORT = 8080
+app.listen(PORT, function(){
+    console.log("App rodandando na porta " + PORT)
+})
