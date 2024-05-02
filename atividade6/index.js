5c34a8e6a775780a28c6048bd343f95069ec2e4a
Efraim <efra10_efraim@hotmail.com>
2024-05-01 23:16:29 -0300

    iniciando atividade 6...

---
+++ atividade6/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const mustacheExpress = require("mustache-express")
+const app = express()
+
+app.engine("html", mustacheExpress())
+app.set("view engine", "html")
+app.set("views", __dirname + "/views")
+
+app.get("/", function(req, res){
+    res.render("index.html")
+})
+
+const PORT = 8080
+app.listen(PORT, function(){
+    console.log("App rodando na porta " + PORT)
+})
